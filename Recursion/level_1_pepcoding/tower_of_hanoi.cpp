58fff9566302e458d4ae66670ff8a0886747c418
amanosan121 <amanosan121@gamil.com>
2021-09-16 21:59:56 +0530

    added tower of hanoi

---
+++ Recursion/level_1_pepcoding/tower_of_hanoi.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+1. There are 3 towers. Tower 1 has n disks, where n is a positive number. 
+Tower 2 and 3 are empty.
+2. The disks are increasingly placed in terms of size such that the smallest disk 
+is on top and largest disk is at bottom.
+3. You are required to 
+    3.1. Print the instructions to move the disks.
+    3.2. from tower 1 to tower 2 using tower 3 
+    3.3. following the rules
+        3.3.1 move 1 disk at a time.
+        3.3.2 never place a smaller disk under a larger disk.
+        3.3.3 you can only move a disk at the top.
+*/
+
+// n - number of disks
+// t1 - source, t2 - destination, t3 - helper tower
+void tower_of_hanoi(int n, int t1, int t2, int t3)
+{
+    // base case --> if we have 0 disks, we cannot do anything.
+    if (n == 0)
+        return;
+
+    // will print instructions to move (n - 1) disks from t1 to t3 using t2
+    tower_of_hanoi(n - 1, t1, t3, t2);
+    // maually moving the remaining 1 disk -> nth disk from t1 to t2
+    cout << n << "[" << t1 << " -> " << t2 << "]\n";
+    // moving the (n-1) disks from t3 to t2 using t1
+    tower_of_hanoi(n - 1, t3, t2, t1);
+}
+
+int main()
+{
+    tower_of_hanoi(3, 10, 11, 12);
+    /*
+    Output should be:
+    1[10 -> 11]
+    2[10 -> 12]
+    1[11 -> 12]
+    3[10 -> 11]
+    1[12 -> 10]
+    2[12 -> 11]
+    1[10 -> 11]
+    */
+    return 0;
+}
